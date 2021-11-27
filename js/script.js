2e4136250abcc4bb48f71be5e1ba7c621a5ff049
Pavel <example@gmail.com>
2021-11-27 16:35:15 +0300

    first task

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-let ggg = 8;
-    hhh = "8";
-console.log(ggg !== hhh);
-console.log('hello world');
+let numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+console.log(numberOfFilms);
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+// console.log(personalMovieDB);
+// console.log(personalMovieDB.count);
+
+let a = prompt("Один из последних просмотренных фильмов?", ''),
+    b = prompt("На сколько оцените его?"),
+    c = prompt("Один из последних просмотренных фильмов?", ''),
+    d = prompt("На сколько оцените его?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
